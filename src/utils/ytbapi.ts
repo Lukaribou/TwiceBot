3d4eee7dcc0266607dbc751926a7a44ffe53bf1d
Lukaribou <lucasperroud17@gmail.com>
2020-06-04 17:21:01 +0200

    Accès aux bonnes données YTB

--- src/utils/ytbapi.ts
+++ src/utils/ytbapi.ts
@@ -1,37 +1,51 @@
 import { google, youtube_v3 } from 'googleapis';
+import * as superagent from 'superagent'
+
+const YOUTUBE_API_URL = 'https://www.googleapis.com/youtube/v3/videos'
 
 export interface IConfigYTBAPI {
-    accessToken: string
-};
+    accessToken: string,
+    youtube: youtube_v3.Youtube
+}
 
 export default class YTBAPI {
     private _data: IConfigYTBAPI = null;
 
     constructor(ytbKey: string) {
         this._data = {
-            accessToken: ytbKey
+            accessToken: ytbKey,
+            youtube: google.youtube({
+                version: 'v3',
+                auth: ytbKey
+            })
         };
-    };
+    }
 
-    public async searchSongs(song: string, maxResults: number = 10): Promise<youtube_v3.Schema$SearchResult[]> {
+    public async getVideosId(videoName: string, maxResults: number = 1): Promise<string[]> {
         return new Promise((resolve, reject) => {
-            google.youtube('v3').search.list({
-                key: this._data.accessToken,
+            this._data.youtube.search.list({
                 part: ['snippet'],
-                q: song,
+                q: videoName,
                 maxResults: maxResults
             })
-            .then(res => {resolve(res.data.items); console.log(res)})
-            .catch((err) => reject(err));
+                .then(res => resolve(res.data.items.map(v => v.id.videoId)))
+                .catch((err) => reject(err));
         });
-    };
+    }
 
-    public async searchSong(song: string) {
-        let temp = await this.searchSongs(song, 1)[0];
-        return temp
-    };
+    public async searchSong(song: string): Promise<youtube_v3.Schema$Video> {
+        return new Promise(async (resolve, reject) => {
+            this._data.youtube.videos.list({
+                part: ['snippet,statistics,contentDetails'],
+                id: (await this.getVideosId(song))
+            }, (err, res) => {
+                if (err) reject(err);
+                resolve(res.data.items.shift());
+            })
+        })
+    }
 
     public getYtbAPIData(): IConfigYTBAPI {
         return this._data;
-    };
-};
+    }
+}
