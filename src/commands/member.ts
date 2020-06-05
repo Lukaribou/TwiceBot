2b77efd80466af567255395e8654c7b82970acf4
Lukaribou <lucasperroud17@gmail.com>
2020-06-05 23:13:53 +0200

    Create member.ts

---
+++ src/commands/member.ts
@@ -0,0 +1,40 @@
+import { Command, CommandParams } from "../utils/structs";
+
+export default class MemberCommand extends Command {
+    name = 'member';
+    desc = 'Displays informations about a member of Twice.';
+    usage = '';
+    categorie: string;
+
+    async execute(args: CommandParams): Promise<void> {
+        
+    }
+}
+
+export function cmToFeets(height: number): string {
+    return (height / 30.48).toFixed(2);
+}
+
+export interface Member {
+    birth: {
+        date: string, // ex: December 29, 1996
+        town: string, // ex: Osaka
+        country: string // ex: Japan (télécharger un drapeau du Japon et le mettre en Thumbnail)
+    },
+    name: {
+        stage: {
+            latin: string, // ex: Sana
+            korean: string // ex: 사나
+        },
+        full: {
+            latin: string,
+            korean: string,
+            native?: string // ex: 湊崎 紗夏
+        },
+        nicknames: string[]
+    },
+    emoji: string,
+    height: number,
+    color: string,
+    position: string[]
+}
