280e5b1769bb68a577562c8692a57830af741cdf
Lukaribou <lucasperroud17@gmail.com>
2020-06-06 18:40:11 +0200

    Update group.ts

--- src/commands/group.ts
+++ src/commands/group.ts
@@ -1,12 +1,31 @@
-import { Command, CommandParams } from "../utils/structs";
+import { Command, CommandParams, twiceInfosdb } from "../utils/structs";
+import { MessageEmbed, MessageAttachment } from "discord.js";
 
 export default class GroupCommand extends Command {
     name = 'group';
     desc = 'Prints information about the Twice group.';
     usage = 'group';
     categorie = 'Twice';
+    aliases = ['band', 'twice']
 
     async execute(args: CommandParams): Promise<void> {
-    
+        const groupImg = new MessageAttachment("res/group.jpg", 'group.jpg');
+        const twiceLogo = new MessageAttachment("res/logo.png", 'logo.png')
+        const jypeLogo = new MessageAttachment("res/jypeLogo.jpg", 'jypeLogo.jpg')
+
+        args.message.channel.send({
+            embed: new MessageEmbed()
+                .setAuthor('Information about Twice', 'attachment://logo.png', 'https://twice.jype.com/')
+                .setImage('attachment://group.jpg')
+                .setThumbnail('attachment://logo.png')
+                .setColor("#ff5fa2")
+                .setDescription('`TWICE` (`트와이스`) is a korean girlsband consisting of `9` members (from oldest to youngest): `' + twiceInfosdb.group.members.join('`, `') + '`')
+                .addField('Start:', '`October 20, 2015` (through the survival show `Sixteen`)', true)
+                .addField('Official colors:', '`Apricot` (`#FCC89B`) & `Neon Magenta` (`#FF5FA2`)', true)
+                .addField('Fandom name:', '`Once`', true)
+                .addField('Labels:', '`JYP Entertainment`, `Warner Music Japan`, `Republic Records`', true)
+                .setFooter('NaJeongMoSaJiMiDaChaeTzu', 'attachment://jypeLogo.jpg'),
+            files: [groupImg, twiceLogo, jypeLogo]
+        });
     }
 }
