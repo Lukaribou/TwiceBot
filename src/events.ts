c3522d4ff88926c33dfb01ecfbcc7aacbfaea61d
Lukaribou <lucasperroud17@gmail.com>
2020-06-08 17:20:03 +0200

    Update events.ts

--- src/events.ts
+++ src/events.ts
@@ -7,10 +7,10 @@ export function onReady(): void {
 }
 
 export async function onMessage(message: Message): Promise<void> {
-    if (message.author.bot) return; // Si l'auteur du message est un bot
-    if (!["text", "news", "shop"].includes(message.channel.type)) return; // Si le message est ailleurs que dans un salon texte sur un serveur en gros
-    if (message.content.trim() === `<@!${bot.user.id}>`) { message.channel.send(`${EMOJIS.RIGHTARROW} \`${bot.config.prefix}help\``); return; }
     if (!message.content.startsWith(bot.config.prefix)) return;
+    if (message.author.bot) { message.channel.send(`${EMOJIS.X} **I don't execute commands from other bots.**`); return; }// Si l'auteur du message est un bot
+    if (!["text", "news", "shop"].includes(message.channel.type)) { message.channel.send(`${EMOJIS.X} **I execute commands only in channels: \`text\`, \`news\` and \`shop\`.**`); return; } // Si le message est ailleurs que dans un salon texte sur un serveur en gros
+    if (message.content.trim() === `<@!${bot.user.id}>`) { message.channel.send(`${EMOJIS.RIGHTARROW} \`${bot.config.prefix}help\``); return; }
 
     const command: string = message.content.split(" ")[0].substring(bot.config.prefix.length);
     const args: string[] = message.content.split(" ").slice(1);
